d39f4d908c0067b1860f0afadd905ef4e9294dff
Solomon <solomonuche@gmail.com>
2024-04-26 01:59:23 +0100

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,17 +2,24 @@
 -- that computes and store the average score for a student
 
 DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-	DECLARE user_score INT;
+	DECLARE total_score INT;
+	DECLARE project_count INT;
+	DECLARE average FLOAT;
 
 	-- get user score
-	SELECT AVG(score) INTO user_score FROM corrections WHERE user_id = user_id;
+	SELECT SUM(score) INTO total_score FROM corrections WHERE corrections.user_id = user_id;
+	SELECT COUNT(*) INTO project_count FROM corrections WHERE corrections.user_id = user_id;
+
+	-- Calculate the average score
+    	SET average = total_score / project_count;
 
 	-- update user average score
 	UPDATE users
-	SET average_score = user_score
+	SET average_score = average
 	WHERE id = user_id;
 END;
 //
