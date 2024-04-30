7169c6dfcb18bcfe268c56316512ba0f25b5b381
solomonuche <solomonuche@gmail.com>
2024-04-30 15:23:53 -0700

    filter

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -9,8 +9,14 @@ def schools_by_topic(mongo_collection, topic):
      a Python function that changes all topics
      of a school document based on the name
     """
-
-    return mongo_collection.find({"topic": topic})
+    topic_filter = {
+            'topics': {
+                '$elemMatch': {
+                    '$eq': topic,
+                    },
+                },
+            }
+    return [doc for doc in mongo_collection.find(topic_filter)]
 
 
 if __name__ == "__main__":
